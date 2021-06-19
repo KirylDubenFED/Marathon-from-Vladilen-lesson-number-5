4ddee67c5f287478d957f3a0c0b70785bf35512e
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-06-19 14:25:40 +0300

    add script

---
+++ app.js
@@ -0,0 +1,15 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screens')
+const timeList = document.querySelector('#time-list')
+
+startBtn.addEventListener('click', (event) => {
+event.preventDefault()
+screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+    if (event.target.classList.contains 
+        ('time-list')) {
+            
+        }
+})
